28387a36b74c698bf19f31db2d6f98af72145789
iam-allend <contact.anurmustakim@gmail.com>
2025-01-22 04:11:49 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/dashboard', 'LayOutController::dashboard');
+$routes->get('/card', 'LayOutController::card');
---
+++ app/Views/backend/layOutTemplate/footer.php
@@ -0,0 +1,40 @@
+        <!-- / Layout page -->
+        </div>
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+        </div>
+        <!-- / Layout wrapper -->
+
+        <div class="buy-now">
+        <a
+        href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
+        target="_blank"
+        class="btn btn-danger btn-buy-now"
+        >Upgrade to Pro</a
+        >
+        </div>
+
+        <!-- Core JS -->
+        <!-- build:js assets/vendor/js/core.js -->
+        <script src="backend/assets/vendor/libs/jquery/jquery.js"></script>
+        <script src="backend/assets/vendor/libs/popper/popper.js"></script>
+        <script src="backend/assets/vendor/js/bootstrap.js"></script>
+        <script src="backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+        <script src="backend/assets/vendor/js/menu.js"></script>
+        <!-- endbuild -->
+
+        <!-- Vendors JS -->
+        <script src="backend/assets/vendor/libs/apex-charts/apexcharts.js"></script>
+
+        <!-- Main JS -->
+        <script src="backend/assets/js/main.js"></script>
+
+        <!-- Page JS -->
+        <script src="backend/assets/js/dashboards-analytics.js"></script>
+
+        <!-- Place this tag in your head or just before your close body tag. -->
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+    </body>
+</html>
