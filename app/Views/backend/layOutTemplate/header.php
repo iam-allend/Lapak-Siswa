bbb408b99d9f920a39411eb39f2b64fe1a033f1d
Muhammadnajwasyarif <najwasyarif56@gmail.com>
2025-02-25 16:25:31 +0700

    ambil update

--- app/Views/backend/layOutTemplate/header.php
+++ app/Views/backend/layOutTemplate/header.php
@@ -1,27 +1,24 @@
-
-
 <!DOCTYPE html>
-<html
-  lang="en">
+<html lang="en">
 
-  <head>
+<head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title><?= $tittle ?></title>
 
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="<?= base_url()?>logo/logo-circle.webp" />
+    <link rel="icon" type="image/x-icon" href="<?= base_url() ?>logo/logo-circle.webp" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
 
     <!--=============== SWEETALERT ===============-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
@@ -30,30 +27,32 @@
 
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="<?= base_url()?>backend/assets/vendor/fonts/boxicons.css" />
+    <link rel="stylesheet" href="<?= base_url() ?>backend/assets/vendor/fonts/boxicons.css" />
     <!-- <link rel="stylesheet" href="backend\assets\vendor\css\my-style.css"> -->
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="<?= base_url()?>backend/assets/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="<?= base_url()?>backend/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="<?= base_url()?>backend/assets/css/demo.css" />
+    <link rel="stylesheet" href="<?= base_url() ?>backend/assets/vendor/css/core.css"
+        class="template-customizer-core-css" />
+    <link rel="stylesheet" href="<?= base_url() ?>backend/assets/vendor/css/theme-default.css"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="<?= base_url() ?>backend/assets/css/demo.css" />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="<?= base_url()?>backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="<?= base_url() ?>backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-    <link rel="stylesheet" href="<?= base_url()?>backend/assets/vendor/libs/apex-charts/apex-charts.css" />
+    <link rel="stylesheet" href="<?= base_url() ?>backend/assets/vendor/libs/apex-charts/apex-charts.css" />
 
     <!-- Page CSS -->
 
     <!-- Helpers -->
-    <script src="<?= base_url()?>backend/assets/vendor/js/helpers.js"></script>
+    <script src="<?= base_url() ?>backend/assets/vendor/js/helpers.js"></script>
 
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="<?= base_url()?>backend/assets/js/config.js"></script>
- 
- 
-    <style>
+    <script src="<?= base_url() ?>backend/assets/js/config.js"></script>
+
+
+    <!-- <style>
       .menu-inner {
         scroll-behavior: smooth; 
         max-height: 90vh !important; /* Atur tinggi maksimum sesuai kebutuhan */
@@ -86,13 +85,12 @@
           scrollbar-color: rgba(0, 0, 0, 0) transparent;
           scrollbar-width: none !important;
       }
-    </style>
-  </head>
+    </style> -->
+</head>
 
-  <body>
+<body>
 
     <!-- Layout wrapper -->
     <div class="layout-wrapper layout-content-navbar">
-      <div class="layout-container">
-        <!-- Menu -->
-
+        <div class="layout-container">
+            <!-- Menu -->
