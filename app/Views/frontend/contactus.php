98b4d8c11f260b633ae62b19274546e22bbcf5cc
iam-allend <contact.anurmustakim@gmail.com>
2025-04-14 08:00:28 +0700

    new final update

--- app/Views/frontend/contactus.php
+++ app/Views/frontend/contactus.php
@@ -33,17 +33,16 @@
         <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
             <h1 class="mb-3">Kontak Kami</h1>
             <p>Ada Pertanyaan? Yuk, Kontak Kami!
-Tim kami dengan senang hati akan menjawab semua pertanyaan kamu. Tinggalkan pesan, dan kami akan segera menghubungimu secepat mungkin!
+                Tim kami dengan senang hati akan menjawab semua pertanyaan kamu. Tinggalkan pesan, dan kami akan segera menghubungimu secepat mungkin!
 
-</p>
+            </p>
         </div>
         <div class="row g-4">
             <div class="col-12">
                 <div class="row gy-4">
                     <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.1s">
                         <div class="bg-light rounded p-3">
-                            <div class="d-flex align-items-center bg-white rounded p-3"
-                                style="border: 1px dashed rgba(0, 185, 142, .3)">
+                            <div class="d-flex align-items-center bg-white rounded p-3" style="border: 1px dashed rgba(0, 185, 142, .3)">
                                 <div class="icon me-3" style="width: 45px; height: 45px;">
                                     <i class="fa fa-map-marker-alt text-primary"></i>
                                 </div>
@@ -53,8 +52,7 @@ Tim kami dengan senang hati akan menjawab semua pertanyaan kamu. Tinggalkan pesa
                     </div>
                     <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.3s">
                         <div class="bg-light rounded p-3">
-                            <div class="d-flex align-items-center bg-white rounded p-3"
-                                style="border: 1px dashed rgba(0, 185, 142, .3)">
+                            <div class="d-flex align-items-center bg-white rounded p-3" style="border: 1px dashed rgba(0, 185, 142, .3)">
                                 <div class="icon me-3" style="width: 45px; height: 45px;">
                                     <i class="fa fa-envelope-open text-primary"></i>
                                 </div>
@@ -64,8 +62,7 @@ Tim kami dengan senang hati akan menjawab semua pertanyaan kamu. Tinggalkan pesa
                     </div>
                     <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
                         <div class="bg-light rounded p-3">
-                            <div class="d-flex align-items-center bg-white rounded p-3"
-                                style="border: 1px dashed rgba(0, 185, 142, .3)">
+                            <div class="d-flex align-items-center bg-white rounded p-3" style="border: 1px dashed rgba(0, 185, 142, .3)">
                                 <div class="icon me-3" style="width: 45px; height: 45px;">
                                     <i class="fa fa-phone-alt text-primary"></i>
                                 </div>
@@ -76,17 +73,14 @@ Tim kami dengan senang hati akan menjawab semua pertanyaan kamu. Tinggalkan pesa
                 </div>
             </div>
             <div class="col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                <iframe class="position-relative rounded w-100 h-100"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.3630783180824!2d110.39943937499694!3d-6.966424193034101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e70f4c8081bffaf%3A0xbb522f8061199b3!2sSMK%20Negeri%2010%20Semarang!5e0!3m2!1sid!2sid!4v1740027626334!5m2!1sid!2sid"
-                    frameborder="0" style="min-height: 400px; border:0;" allowfullscreen="" aria-hidden="false"
-                    tabindex="0"></iframe>
-                   
+                <iframe class="position-relative rounded w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.3630783180824!2d110.39943937499694!3d-6.966424193034101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e70f4c8081bffaf%3A0xbb522f8061199b3!2sSMK%20Negeri%2010%20Semarang!5e0!3m2!1sid!2sid!4v1740027626334!5m2!1sid!2sid" frameborder="0" style="min-height: 400px; border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
             </div>
             <div class="col-md-6">
                 <div class="wow fadeInUp" data-wow-delay="0.5s">
-                    <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form
+                    <!-- <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form
                         with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're
-                        done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
+                        done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p> -->
                     <form>
                         <div class="row g-3">
                             <div class="col-md-6">
@@ -109,8 +103,7 @@ Tim kami dengan senang hati akan menjawab semua pertanyaan kamu. Tinggalkan pesa
                             </div>
                             <div class="col-12">
                                 <div class="form-floating">
-                                    <textarea class="form-control" placeholder="Leave a message here" id="message"
-                                        style="height: 150px"></textarea>
+                                    <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 150px"></textarea>
                                     <label for="message">Message</label>
                                 </div>
                             </div>
